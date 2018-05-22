6d21502921c176218f37f270d508dc64a8962c6b
Minh Tam <minhtamho91@gmail.com>
2018-05-22 15:39:36 +0700

    edit dynamic baseURL

--- client/src/CommonConfig.js
+++ client/src/CommonConfig.js
@@ -11,8 +11,9 @@ const commonConfig = {
             return decoded;
         });
     },
+    baseURL: window.location.protocol + '//' + window.location.hostname + ':5000/'
     //baseURL: 'http://vps159788.vps.ovh.ca:5000/'
-    baseURL: 'http://localhost:5000/'
+    //baseURL: 'http://localhost:5000/'
 }
 
 export default commonConfig;
--- client/src/axiosInst.js
+++ client/src/axiosInst.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const axiosInst = axios.create({
     //baseURL: 'http://vps159788.vps.ovh.ca:5000'
-    baseURL: 'http://localhost:5000'
+    //baseURL: 'http://localhost:5000'
+    baseURL: window.location.protocol + '//' + window.location.hostname + ':5000/'
 });
 
 axiosInst.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
