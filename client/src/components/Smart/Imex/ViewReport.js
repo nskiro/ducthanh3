21dfca1036febeb4efc1406bc00738b4dccc4509
Minh Tam <minhtamho91@gmail.com>
2018-05-23 10:23:12 +0700

    Add Imex Avatar

--- client/src/components/Smart/Imex/ViewReport.js
+++ client/src/components/Smart/Imex/ViewReport.js
@@ -4,6 +4,8 @@ import { Row, Col } from 'react-bootstrap';
 import Aux from '../../../hoc';
 import DeptInfo from '../../Dumb/DeptInfo/DeptInfo';
 
+import ImexAvatar from '../../../assets/images/dept/imex.jpg';
+
 class ViewReport extends Component {
     render(){        
         return(
@@ -15,7 +17,8 @@ class ViewReport extends Component {
                 </Row>
                 <Row className="show-grid">
                     <Col xs={12} sm={12}>
-                        <DeptInfo 
+                        <DeptInfo
+                            avatar = {ImexAvatar}
                             title="Import - Export"
                             head = "Mr. Thanh Huynh"
                             email = "phuocthanh@ducthanh3.com.vn"
